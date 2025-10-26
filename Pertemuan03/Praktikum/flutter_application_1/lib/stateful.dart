129a21af0a38cbdfe9fa94a1e07a2cd3fae716ba
Savanazahra18 <zahrasavana999@gmail.com>
2025-10-26 21:45:08 +0700

    Praktikum dan Output

---
+++ Pertemuan03/Praktikum/flutter_application_1/lib/stateful.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyStateful extends StatefulWidget {
+  const MyStateful({super.key});
+
+  @override
+  State<MyStateful> createState() => _MyStatefulState();
+}
+
+class _MyStatefulState extends State<MyStateful> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Hello, Stateful'))),
+    );
+  }
+}
